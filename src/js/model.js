e16359170af1008462261a784b7edb385d9b2827
Alex <a.gachegov@agatgroup.com>
2024-04-12 17:50:10 +0300

    layout

---
+++ src/js/model.js
@@ -0,0 +1,7 @@
+/**
+ * @class Model 
+ * Menages the data for the application / communicates with the controller
+ */
+class Model {
+  constructor() {}
+}
---
+++ src/js/view.js
@@ -0,0 +1,7 @@
+/**
+ * class View
+ * Manages the user interface / communicates with the controller
+ */
+class View {
+    constructor() {}
+}
